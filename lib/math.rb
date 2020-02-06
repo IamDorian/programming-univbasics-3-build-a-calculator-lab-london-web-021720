07a993d88ce396d272a6f769267f45260d684c8d
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-06 17:29:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ return addition
 end
 
 def subtraction(num1, num2)
-
+subtraction = num2 - num1
+return subtraction
 end
 
 def division(num1, num2)
-
+division = num2 / num1
+return division
 end
 
 def multiplication(num1, num2)
-
+multiplication = num1 * num2
+return multiplication
 end
 
 def modulo(num1, num2)
