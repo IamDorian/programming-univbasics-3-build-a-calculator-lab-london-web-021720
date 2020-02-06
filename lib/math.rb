b61cdc7c13b264c7493042336161a4a031ae3a68
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-06 17:40:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,21 +9,21 @@ return subtraction
 end
 
 def division(num1, num2)
-division = num2 / num1
+division == num2 / num1
 return division
 end
 
 def multiplication(num1, num2)
-multiplication = num1 * num2
+multiplication == num1 * num2
 return multiplication
 end
 
 def modulo(num1, num2)
-modulo = num2 % num1
+modulo == num2 % num1
 return modulo
 end
 
 def square_root(num)
-square_root = Math.sqrt()
+square_root == Math.sqrt()
 return square_root
 end
